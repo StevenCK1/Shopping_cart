3334e1218fc8faabc7d0fd1165930fcc2c6622c9
Steven Cheung <stevencheung@hotmail.co.uk>
2022-05-24 21:04:24 +0100

    completed cart logic functionalities

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ class UI {
           />
           <button class="bag-btn" data-id=${product.id}>
             <i class="fas fa-shopping-cart"></i>
-            add to bag
+            add to cart
           </button>
           <h3>${product.title}</h3>
           <h4>${product.price}</h4>
@@ -94,7 +94,7 @@ class UI {
         // save cart in local storage
         Storage.saveCart(cart);
         // set cart values
-        this.setCartValues(cart);
+        this.setCartValues(cart); // this keyword references the object UI, since getBagButtons is a method in the UI object // if there is a regular function (not part of an object, ie not method of an object), this keyword will reference the global object (window, global)
         // display cart item
         this.addCartItem(cartItem);
         // show cart
@@ -152,6 +152,36 @@ class UI {
       this.clearCart();
     });
     // cart functionality
+    cartContent.addEventListener("click", (event) => {
+      //event bubbling
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
+      }
+    });
   }
   clearCart() {
     let cartItems = cart.map((item) => item.id);
@@ -168,7 +198,7 @@ class UI {
     Storage.saveCart(cart);
     let button = this.getSingleButton(id);
     button.disabled = false;
-    button.innerHTML = `<i class="fas fa-shopping-cart></i>add to cart`;
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
   }
   getSingleButton(id) {
     return buttonsDOM.find((button) => button.dataset.id === id);
